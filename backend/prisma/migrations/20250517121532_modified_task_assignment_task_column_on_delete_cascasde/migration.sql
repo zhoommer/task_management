7f14a2fb1822ce70a3a8c9cc251605a7af7954ff
Akif Fazil GUVEN <62653939+zhoommer@users.noreply.github.com>
2025-05-24 20:16:25 +0300

    Delete backend/prisma/migrations directory

--- backend/prisma/migrations/20250517121532_modified_task_assignment_task_column_on_delete_cascasde/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "TaskAssignment" DROP CONSTRAINT "TaskAssignment_taskId_fkey";
-
--- AddForeignKey
-ALTER TABLE "TaskAssignment" ADD CONSTRAINT "TaskAssignment_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- backend/prisma/migrations/20250523210637_init_0/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Task" ALTER COLUMN "status" SET DEFAULT 'waiting';
